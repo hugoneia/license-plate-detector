893ce2930e93ba652cac7c978cc92c9bc9fb8a12
hugoneia <hugone.blog@gmail.com>
2026-09-03 15:38:23 +0200

    Actualizar load-env.js

--- scripts/load-env.js
+++ scripts/load-env.js
@@ -32,18 +32,3 @@ if (fs.existsSync(envPath)) {
     }
   });
 }
-
-// Map system variables to Expo public variables
-const mappings = {
-  VITE_APP_ID: "EXPO_PUBLIC_APP_ID",
-  VITE_OAUTH_PORTAL_URL: "EXPO_PUBLIC_OAUTH_PORTAL_URL",
-  OAUTH_SERVER_URL: "EXPO_PUBLIC_OAUTH_SERVER_URL",
-  OWNER_OPEN_ID: "EXPO_PUBLIC_OWNER_OPEN_ID",
-  OWNER_NAME: "EXPO_PUBLIC_OWNER_NAME",
-};
-
-for (const [systemVar, expoVar] of Object.entries(mappings)) {
-  if (process.env[systemVar] && !process.env[expoVar]) {
-    process.env[expoVar] = process.env[systemVar];
-  }
-}
