1e14bd508a9bac8bfe1d134cfea6243288431dca
hugoneia <hugone.blog@gmail.com>
2026-09-14 16:10:27 +0200

    Actualizar versión a 2.1.9

--- app.config.ts
+++ app.config.ts
@@ -42,7 +42,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "2.1.8",
+  version: "2.1.9",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -64,7 +64,7 @@ const config: ExpoConfig = {
     },
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
-    versionCode: 20108,
+    versionCode: 20109,
     package: env.androidPackage,
     permissions: ["POST_NOTIFICATIONS", "READ_EXTERNAL_STORAGE", "WRITE_EXTERNAL_STORAGE"],
     intentFilters: [
